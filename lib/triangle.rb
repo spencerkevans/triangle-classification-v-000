c64b1019c6efb8366e6e579250d4d4d7f2a9f6e3
spensation <spencevans@gmail.com>
2020-02-17 19:11:11 -0500

    working..

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,19 @@
 class Triangle
-  # write code here
+  attr_accessor :side_a, :side_b, :side_c
+
+  def initialize(side_a, side_b, side_c)
+  	@side_a = side_a
+  	@side_b = side_b
+  	@side_c = side_c
+  end
+
+  def kind
+  	if side_a == side_b && side_b == side_c
+  		:equilateral
+  	elsif side_a == side_b || side_b == side_c || side_a == side_c
+  		:isosceles
+  	else
+  		:scalene
+  	end
+  end
 end
